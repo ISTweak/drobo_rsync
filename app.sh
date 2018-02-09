972f070cc8f4c052be660d7d40ebbab306a8656d
ISTweak <istweak@d-o-b.org>
2018-02-09 09:45:33 +0900

    update 3.1.3

--- app.sh
+++ app.sh
@@ -1,6 +1,6 @@
 ### RSYNC ###
 _build_rsync() {
-local VERSION="3.1.2"
+local VERSION="3.1.3"
 local FOLDER="rsync-${VERSION}"
 local FILE="${FOLDER}.tar.gz"
 local URL="https://rsync.samba.org/ftp/rsync/src/${FILE}"
